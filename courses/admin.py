2c564a9596f5fa4c5a50f9200cbb8c2c2bcb2e92
Hossein Assaran <hosseinassaran@Hosseins-MacBook-Pro.local>
2018-09-03 19:08:42 +0430

    Compelete model and admin files

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Lesson, Page
+
+
+class LessonInLine(admin.StackedInline):
+    model = Lesson
+
+class PageInLine(admin.StackedInline):
+    model = Page
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title']
+    inlines = [LessonInLine]
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    inlines = [PageInLine]
+
+@admin.register(Page)
+class FlashCardAdmin(admin.ModelAdmin):
+    list_display = ['key']
--- courses/models.py
+++ courses/models.py
@@ -5,3 +5,27 @@ class Course(models.Model):
     iconAddr = models.CharField(max_length=256)
     relAddr = models.CharField(max_length=256)
     numInRow = models.IntegerField(null=True)
+
+    class Meta:
+        ordering = ('title', )
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey('Course', related_name='lessons', on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100)
+    relAddr = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
+
+class Page(models.Model):
+    lesson = models.ForeignKey('Lesson', related_name='pages', on_delete=models.CASCADE, null=True)
+    key = models.CharField(max_length=100)
+    icon = models.CharField(max_length=256)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title    
+    
